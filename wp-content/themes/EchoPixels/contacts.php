0fb2601ee98c8406c6ea4c2ef68801a5b9206fcc
bacaAlexandre <Alexandre76b@aol.com>
2017-05-13 14:03:15 +0200

    creation et lien de la page contacts

---
+++ wp-content/themes/EchoPixels/contacts.php
@@ -0,0 +1,38 @@
+<?php
+
+  /*
+  Template Name: Contacts
+  */
+  get_header();
+ ?>
+ <section>
+   <p>Page contacts</p>
+ </section>
+
+
+ <section>
+
+ </section>
+
+
+
+ <section>
+
+ </section>
+
+
+
+ <section>
+
+ </section>
+
+
+
+
+ <section>
+
+ </section>
+
+ <?php
+ get_footer();
+  ?>
