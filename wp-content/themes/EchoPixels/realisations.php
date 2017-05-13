3c4bced2e8405cc309a9263614ea2c551e4056bf
bacaAlexandre <Alexandre76b@aol.com>
2017-05-13 14:01:28 +0200

    creation et lien vers page realisation

---
+++ wp-content/themes/EchoPixels/realisations.php
@@ -0,0 +1,38 @@
+<?php
+
+  /*
+  Template Name: Realisations
+  */
+  get_header();
+ ?>
+ <section>
+   <p>Page realisation</p>
+ </section>
+
+
+ <section>
+
+ </section>
+
+
+
+ <section>
+
+ </section>
+
+
+
+ <section>
+
+ </section>
+
+
+
+
+ <section>
+
+ </section>
+
+ <?php
+ get_footer();
+  ?>
