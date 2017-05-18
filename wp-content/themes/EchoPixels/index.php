6e9e4c90361a0a2d08a2345fdec963eef9a0f90c
bertrandweb <feuille.bertrand@orange.fr>
2017-05-18 15:05:18 +0200

    Ajout html (index) et test git

--- wp-content/themes/EchoPixels/index.php
+++ wp-content/themes/EchoPixels/index.php
@@ -5,8 +5,25 @@ Template Name: Accueil
   get_header();
  ?>
  <section>
-   <p>accueil</p>
+  <div class="banniere">
+   <img src="./wp-includes/images/echopixels/fond_accueil.jpg" alt="banniere_bienvenue">
+      <h1>Bienvenue</h1>
+      <p>sur le site de l'association des anciens élèves de la formation WebForce 3 Développeur / Intégrateur Web</p>
+      <h2>Une association numérique au coeur du Bassin Dieppois</h2>
+  </div>
  </section>
+
+ <section>
+    <div class="presentation">
+        <div class="img_association">
+            <img src="./wp-includes/images/echopixels/groupe_asso.jpg" alt="photo de groupe de l'association">
+        </div>
+        <div>
+            <p>L'association lorem ipsum</p>
+        </div>
+    </div>
+ </section>
+
 
 
  <section>
@@ -16,13 +33,11 @@ Template Name: Accueil
 
 
  <section>
-
- </section>
-
-
-
- <section>
-
+    <h4>CONTACTEZ-NOUS</h4>
+    <img src="./wp-includes/images/echopixels/mail.jpg">
+    <a mailto="echopixels@gmail.com">echopixels@gmail.com</a>
+    <br>
+     <p>ECH<O> PIXELS - Tout droit réservés - <a href="#">Mention légales</a></O></p>
  </section>
 
 
