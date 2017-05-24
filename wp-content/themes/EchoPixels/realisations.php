4659b6db8f57283eef5b0c3dec625c5358e09826
bacaAlexandre <Alexandre76b@aol.com>
2017-05-24 17:49:12 +0200

    logo page realisation + overlay

--- wp-content/themes/EchoPixels/realisations.php
+++ wp-content/themes/EchoPixels/realisations.php
@@ -5,33 +5,43 @@
   */
   get_header();
  ?>
- <section>
-   <p>Page realisation</p>
- </section>
+ <main>
+   <div class="page_logo">
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/echoPixels.png" alt="">
+     </div>
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div>
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div><div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div><div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div><div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div>
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div>
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div>
+     <div class="wrapper_logo">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     </div>
+   </div>
+   <div class="overlay">
+   </div>
+   <div class="description">
+     <img src="<?php echo get_template_directory_uri(); ?>/image/logo/echoPixels.png" alt="">
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+   </div>
+ </main>
 
 
- <section>
-
- </section>
-
-
-
- <section>
-
- </section>
-
-
-
- <section>
-
- </section>
-
-
-
-
- <section>
-
- </section>
 
  <?php
  get_footer();
