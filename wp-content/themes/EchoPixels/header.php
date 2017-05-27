02f7aeca1c5e0044dd089b8a2b7669a1d57cfca4
bacaAlexandre <Alexandre76b@aol.com>
2017-05-27 16:27:21 +0200

    html+css page realisation

--- wp-content/themes/EchoPixels/header.php
+++ wp-content/themes/EchoPixels/header.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL | E_STRICT)
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Exo:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Exo:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">    
     <?php wp_head(); ?>
     <title>Echo Pixels</title>
   </head>
--- wp-content/themes/EchoPixels/realisations.php
+++ wp-content/themes/EchoPixels/realisations.php
@@ -5,39 +5,63 @@
   */
   get_header();
  ?>
- <main>
-   <div class="page_logo">
-     <div class="wrapper_logo">
+ <main class="realisation_content">
+   <h2 class="vert">Les différents sites réalisés par l'équipe d'EchoPixels</h2>
+   <div class="realisation_wrapper">
+     <div class="realisation_col_left">
        <img src="<?php echo get_template_directory_uri(); ?>/image/logo/echoPixels.png" alt="">
+       <ul class="bleu">
+         <h4>Equipe</h4>
+         <li>Chef de projet : <span class="vert">Baca Alexandre</span></li>
+         <li>Développeur/Intégrateur web : <span class="vert">Baca Alexandre</span></li>
+         <li>Développeur/Intégrateur web : <span class="vert">Feuille Bertrand</span></li>
+         <li>Web designer : <span class="vert">Blavet Nicolas</span></li>
+         <li>Redatrice web : <span class="vert">Halbout Carine</span></li>
+       </ul>
      </div>
-     <div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div>
-     <div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div><div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div><div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div><div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div>
-     <div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div>
-     <div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
-     </div>
-     <div class="wrapper_logo">
-       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+     <div class="realisation_col_right">
+       <h3 class="bleu">EchoPixels</h3>
+       <p class="bleu">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       <a href="#" target="_blank">Aller sur le site </a>
      </div>
    </div>
-   <div class="overlay">
-   </div>
-   <div class="description">
-     <img src="<?php echo get_template_directory_uri(); ?>/image/logo/echoPixels.png" alt="">
-     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
+   <div class="realisation_wrapper">
+     <div class="realisation_col_left">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/fond_de_placard.png" alt="">
+       <ul class="bleu">
+         <h4>Equipe</h4>
+         <li>Chef de projet : <span class="vert">Goyer Paul</span></li>
+         <li>Développeur web : <span class="vert">Goyer Paul</span></li>
+         <li>Développeur web : <span class="vert">Rimbert Sammy</span></li>
+         <li>Intégrateur web : <span class="vert">Lerquet Théo</span></li>
+         <li>Intégrateur web : <span class="vert">Baca Alexandre</span></li>
+       </ul>
+     </div>
+     <div class="realisation_col_right">
+       <h3 class="bleu">Fond de placard</h3>
+       <p class="bleu">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       <a href="http://fond-de-placard.baca-alexandre.fr/" target="_blank">Aller sur le site </a>
+     </div>
+   </div>
+
+   <div class="realisation_wrapper">
+     <div class="realisation_col_left">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/logo/.png" alt="">
+       <ul class="bleu">
+         <h4>Equipe</h4>
+         <li>Chef de projet : <span class="vert">Feuille Bertrand</span></li>
+         <li>Développeur web : <span class="vert">Feuille Bertrand</span></li>
+         <li>Développeur web : <span class="vert">Blavet Nicolas</span></li>
+         <li>Intégrateur web : <span class="vert">Halbout Carine</span></li>
+         <li>Intégrateur web : <span class="vert">Hedouin Quentin</span></li>
+       </ul>
+     </div>
+     <div class="realisation_col_right">
+       <h3 class="bleu">Ikedori</h3>
+       <p class="bleu">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       <a href="#" target="_blank">Aller sur le site </a>
+     </div>
    </div>
  </main>
 
