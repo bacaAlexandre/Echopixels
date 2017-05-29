069adcffe0a6e82b1401dae6335dc74ee636bfd6
bertrandweb <feuille.bertrand@orange.fr>
2017-05-29 16:09:51 +0200

    Correction bug de lien banniere page contact (en responsive). Correction bug de disparition du menu en non responsive. Amélioration du rendu en responsive

--- wp-content/themes/EchoPixels/js/custom.js
+++ wp-content/themes/EchoPixels/js/custom.js
@@ -1,12 +1,10 @@
 /* Menu responsive */
 
 (function($) {
-$( ".menu_header" ).hide();
-
-    $( ".menu_header_responsive" ).click(function() {
-        $( ".menu_header" ).slideToggle( "slow" );
- });
-
+    $(".menu_header_responsive").click(function () {
+        $( ".menu_header" ).css("display:block");
+        $( ".menu_header" ).slideToggle("slow");
+    });
 })(jQuery);
 
 
