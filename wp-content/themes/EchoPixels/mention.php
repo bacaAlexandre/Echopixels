c4fe7c38cb725109e8b45fc72f0220b978e8e37f
bertrandweb <feuille.bertrand@orange.fr>
2017-05-30 14:53:59 +0200

    Ajout de la page mentions légales

--- wp-content/themes/EchoPixels/mention.php
+++ wp-content/themes/EchoPixels/mention.php
@@ -5,8 +5,49 @@ echo $post->ID;
   */
   get_header();
  ?>
- <section>
-   <p>Mention légales</p>
+
+<section>
+    <div class="banniere">
+        <img src="<?php echo get_template_directory_uri(); ?>/image/fond_contact.jpg" alt="fond_membres">
+    </div>
+    <div class="banniere_responsive">
+        <img src="<?php echo get_template_directory_uri(); ?>/image/fond_contact_responsive.jpg" alt="banniere_bienvenue">
+    </div>
+</section>
+ <section class="mentions">
+   <h2>Mention légales</h2>
+
+   <p>
+       <strong>Personne morale éditrice de ce site</strong>
+   </p>
+   <p>
+       Le présent site www.echopixels.com est la propriété de l’Association Echo Pixels dont le siège est situé 6-10 Rue Verdier Monetti, DMI ex-site Regma, 76880 Arques-la-Bataille email : echopixels@gmail.com
+   </p>
+
+   <p>
+       Directeur de publication : Tristan Parcelier
+   </p>
+
+   <p>
+       <strong>Hébergement du site</strong>
+   </p>
+
+   <p>
+         Le site est hébergé sur :
+         SAS OVH –
+         140, quai du Sartel 59100 ROUBAIX
+         Tel : 0 899 701 761
+         Fax : 03 20 20 09 58
+         Email : support@ovh.com.
+         Nom de domaine : influenceursduweb.org
+   </p>
+
+   <p>
+       <strong>Propriété intellectuelle</strong>
+   </p>
+   <p>
+         La structure générale, ainsi que les textes, images, et de manière générale, tous les éléments composant ce site sont la propriété de l'Association Echo Pixels et sont en tant que tels, protégés par les lois en vigueur, notamment en matière de droit d'auteur et de propriété intellectuelle. Toute reproduction, représentation, modification, publication, adaptation totale ou partielle de ce site ou d'un ou de plusieurs de ses composants, par quelque procédé que ce soit, sans autorisation expresse de l'Association est interdite et constituerait une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.
+   </p>
  </section>
 
 
