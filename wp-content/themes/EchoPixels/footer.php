d84e52363f5dbb3ffbb3381b989b1bdd211ffba0
bacaAlexandre <Alexandre76b@aol.com>
2017-06-02 15:24:41 +0200

    modification email dans footer

--- wp-content/themes/EchoPixels/footer.php
+++ wp-content/themes/EchoPixels/footer.php
@@ -3,10 +3,10 @@
      <div class="style_footer">
       <div class="footer_mail">
        <h4>CONTACTEZ-NOUS</h4>
-       <i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-       <a href="mailto:echopixels@gmail.com">echopixels@gmail.com</a>
+
       </div>
       <div class="lien_association">
+        <a href="mailto:echopixels@gmail.com"><i class="fa fa-envelope-o " aria-hidden="true"></i></a>
         <a href="https://twitter.com/EchoPixels?lang=fr" target="_blank" class="icone bleu"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
         <a href="https://www.facebook.com/groups/echopixels/" target="_blank" class="icone bleu"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
       </div>
