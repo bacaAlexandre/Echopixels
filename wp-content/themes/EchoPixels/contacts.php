728bb57d3f6bf77958c27634e2d74db44392cbed
carilouise <carlg140476510>
2017-06-07 10:19:05 +0200

    ajout texte page contact

--- wp-content/themes/EchoPixels/contacts.php
+++ wp-content/themes/EchoPixels/contacts.php
@@ -14,6 +14,9 @@
     <img src="<?php echo get_template_directory_uri(); ?>/image/fond_contact_responsive.jpg" alt="banniere_bienvenue">
    </div>
   </section>
+  <section>
+      <h3> Vous souhaitez en savoir plus sur notre association ou nous soumettre un projet de site internet ? Merci de remplir le formulaire de contact, nous ne manquerons pas de revenir vers vous ! </h3>
+  </section>
   <section class="formulaire_contact">
    <h1>Contacter l'association Echo Pixels</h1>
       <div>
